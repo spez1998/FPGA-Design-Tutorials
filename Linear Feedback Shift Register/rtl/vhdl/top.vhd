5438e9d91fb4f707caf026ae0cf76435c6f28eb0
spez1998 <samman30@live.com>
2021-10-18 00:58:30 +0100

    Starting to connect entities together.

--- Linear Feedback Shift Register/rtl/vhdl/top.vhd
+++ Linear Feedback Shift Register/rtl/vhdl/top.vhd
@@ -3,35 +3,84 @@ use IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity top is
-    Generic (WIDTH: 15);
-    Port    (reset, clk : in  STD_LOGIC;
-             Q          : out STD_LOGIC_VECTOR (WIDTH-1 downto 0));
+    Generic (LFSR_WIDTH: positive := 15;
+             HEX_WIDTH : positive := 4);
+    Port    (reset, clk : in  STD_LOGIC);
 end top;
 
 architecture behavioral of top is
 
     component lfsr
-        port(CLK, RESET : in  std_logic;
-             Q          : out std_logic_vector (WIDTH-1 downto 0));
+        port(CLK, RESET : in  std_logic);
     end component;
 
-    signal counter  : std_logic_vector(22 downto 0);
+    component counter
+        port(CLK, RST : in std_logic;
+             C        : out std_logic_vector (1 downto 0));
+    end component;
+
+    component mux
+        port(D0 : in  STD_LOGIC_VECTOR (HEX_WIDTH-1 downto 0);
+             D1 : in  STD_LOGIC_VECTOR (HEX_WIDTH-1 downto 0);
+             D2 : in  STD_LOGIC_VECTOR (HEX_WIDTH-1 downto 0);
+             D3 : in  STD_LOGIC_VECTOR (HEX_WIDTH-1 downto 0);
+             S  : in  STD_LOGIC_VECTOR (1 downto 0);
+             Y  : out STD_LOGIC_VECTOR (HEX_WIDTH-1 downto 0)
+    );
+
+    end component;
+
+    signal slow_counter  : std_logic_vector(22 downto 0);
+    signal med_counter   : std_logic_vector(16 downto 0);
+    
     signal slow_clk : std_logic;
+    signal med_clk  : std_logic;
+
+    signal S : std_logic_vector(1 downto 0);
+    signal hex_nibble : std_logic_vector(3 downto 0);
+
+    signal 7seg_anode : std_logic;
+
+    signal lfsr_out     : std_logic_vector(LFSR_WIDTH downto 0);
+    signal lfsr_out0    : std_logic_vector(HEX_WIDTH downto 0);
+    signal lfsr_out1    : std_logic_vector(HEX_WIDTH downto 0);
+    signal lfsr_out2    : std_logic_vector(HEX_WIDTH downto 0);
+    signal lfsr_out3    : std_logic_vector(HEX_WIDTH downto 0);
+    signal lfsr_out_cur : std_logic_vector(HEX_WIDTH downto 0);
+
+    signal 
 
 begin
 
-    unit: lfsr port map (CLK => slow_clk, RESET => reset, Q => Q);
+    unit: lfsr port map (CLK => slow_clk, RESET => reset, Q => lfsr_out);
+    unit: counter port map (CLK => med_clk, RST => reset, C => S);
+    unit: dec_l port map (S => S, Y => 7seg_anode);
+    unit: mux port map (D0 => lfsr_out0,
+                        D1 => lfsr_out1,
+                        D2 => lfsr_out2,
+                        D3 => lfsr_out3,
+                        S => S,
+                        Y => lfsr_out_cur);
 
     -- 50MHz clock divided by 2^23 gives ~6Hz slow clock
+    -- 50MHz clock divided by 2^17 gives ~380Hz med clock
     process(clk, reset)
     begin
         if reset = '1' then
-            counter <= (others => '0');
+            slow_counter <= (others => '0');
+            med_counter  <= (others => '0');
         elsif rising_edge(clk) then
-            counter <= counter + 1;
+            slow_counter <= slow_counter + 1;
+            med_counter  <= med_counter + 1;
         end if;
     end process;
 
-    slow_clk <= counter(22)
+    slow_clk <= slow_counter(22)
+    med_clk  <= med_counter(16)
+
+    lfsr_out3 <= lfsr_out(15 downto 12);
+    lfsr_out2 <= lfsr_out(11 downto 8);
+    lfsr_out1 <= lfsr_out(7 downto 4);
+    lfsr_out0 <= lfsr_out(3 downto 0);
 
 end behavioral;
