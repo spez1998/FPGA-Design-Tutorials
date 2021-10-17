1ca5ec55d0c4a08878e3ba509e25b63d8b656776
spez1998 <samman30@live.com>
2021-10-18 00:14:06 +0100

    Created RTL for active-low 2-to-4 line decoder.

---
+++ Linear Feedback Shift Register/rtl/vhdl/dec_l.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity dec_l is
+    port    (S  : in   STD_LOGIC_VECTOR (1 downto 0);
+             Y  : out  STD_LOGIC
+    );
+
+end dec_l;
+
+architecture behav of dec_l is
+
+begin
+
+    process(S, Y) begin
+        if(S = "00") then
+            Y <= "1110";
+        elsif(S = "01") then
+            Y <= "1101";
+        elsif(S = "10") then
+            Y <= "1011";
+        elsif(S = "11") then
+            Y <= "0111";
+        end if;
+    end process;
+
+
+end architecture behav;
