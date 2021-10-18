9d784a2dab6e32d2553af3a37ed3436d0b7cd608
spez1998 <samman30@live.com>
2021-10-18 01:24:57 +0100

    Finished connecting modules together. Time for testing.

--- Linear Feedback Shift Register/rtl/vhdl/top.vhd
+++ Linear Feedback Shift Register/rtl/vhdl/top.vhd
@@ -3,9 +3,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity top is
-    Generic (LFSR_WIDTH: positive := 15;
-             HEX_WIDTH : positive := 4);
-    Port    (reset, clk : in  STD_LOGIC);
+    Generic (LFSR_WIDTH : positive := 15;
+             HEX_WIDTH  : positive := 4);
+    Port    (reset, clk   : in  STD_LOGIC;
+             anode_7seg   : out STD_LOGIC;
+             cathode_7seg : out STD_LOGIC_VECTOR(7 downto 0));
 end top;
 
 architecture behavioral of top is
@@ -27,9 +29,18 @@ architecture behavioral of top is
              S  : in  STD_LOGIC_VECTOR (1 downto 0);
              Y  : out STD_LOGIC_VECTOR (HEX_WIDTH-1 downto 0)
     );
-
     end component;
 
+    component dec_l
+        port(S : in STD_LOGIC_VECTOR (1 downto 0);
+             Y: STD_LOGIC_VECTOR (3 downto 0));
+    end component;
+
+    component dec_7seg
+    port (D_IN  : in STD_LOGIC_VECTOR (3 downto 0);
+          D_OUT : out STD_LOGIC_VECTOR (7 downto 0));
+    end component;
+    
     signal slow_counter  : std_logic_vector(22 downto 0);
     signal med_counter   : std_logic_vector(16 downto 0);
     
@@ -38,8 +49,6 @@ architecture behavioral of top is
 
     signal S : std_logic_vector(1 downto 0);
     signal hex_nibble : std_logic_vector(3 downto 0);
-
-    signal 7seg_anode : std_logic;
 
     signal lfsr_out     : std_logic_vector(LFSR_WIDTH downto 0);
     signal lfsr_out0    : std_logic_vector(HEX_WIDTH downto 0);
@@ -54,7 +63,8 @@ begin
 
     unit: lfsr port map (CLK => slow_clk, RESET => reset, Q => lfsr_out);
     unit: counter port map (CLK => med_clk, RST => reset, C => S);
-    unit: dec_l port map (S => S, Y => 7seg_anode);
+    unit: dec_l port map (S => S, Y => anode_7seg);
+    unit: dec_7seg port map (D_IN => lfsr_out_cur, D_OUT => cathode_7seg);
     unit: mux port map (D0 => lfsr_out0,
                         D1 => lfsr_out1,
                         D2 => lfsr_out2,
@@ -78,6 +88,7 @@ begin
     slow_clk <= slow_counter(22)
     med_clk  <= med_counter(16)
 
+    -- Assign LFSR output nibbles to discrete signals
     lfsr_out3 <= lfsr_out(15 downto 12);
     lfsr_out2 <= lfsr_out(11 downto 8);
     lfsr_out1 <= lfsr_out(7 downto 4);
