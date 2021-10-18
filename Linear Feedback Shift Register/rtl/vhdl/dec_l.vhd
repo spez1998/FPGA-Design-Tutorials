7b83ec15349c3d230fa1f9c9a35971d1e33f08bb
spez1998 <samman30@live.com>
2021-10-18 01:22:19 +0100

    Corrected incorrect syntax.

--- Linear Feedback Shift Register/rtl/vhdl/dec_l.vhd
+++ Linear Feedback Shift Register/rtl/vhdl/dec_l.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity dec_l is
     port    (S  : in   STD_LOGIC_VECTOR (1 downto 0);
-             Y  : out  STD_LOGIC
+             Y  : out  STD_LOGIC_VECTOR (3 downto 0)
     );
 
 end dec_l;
