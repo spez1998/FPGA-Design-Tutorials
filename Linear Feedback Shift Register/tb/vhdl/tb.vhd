42128f43f6c08130ed22e721f78874bd687fe962
spez1998 <samman30@live.com>
2021-10-18 11:24:39 +0100

    Edited testbench to reflect full design.

--- Linear Feedback Shift Register/tb/vhdl/tb.vhd
+++ Linear Feedback Shift Register/tb/vhdl/tb.vhd
@@ -6,19 +6,23 @@ end lfsr_tb;
 
 architecture behavior of lfsr_tb is
 
-    component lfsr
-    Port(clk   : IN std_logic;
-         reset : IN std_logic;
-         Q     : OUT std_logic_vector(15 downto 0));
+    component top
+    Port(clk, reset   : IN std_logic;
+         anode_7seg   : OUT std_logic;
+         cathode_7seg : OUT std_logic_vector(7 downto 0));
     end component;
 
-    signal clk   : std_logic := '0';
-    signal reset : std_logic := '0';
-    signal Q     : std_logic_vector(15 downto 0);
+    signal clk          : std_logic := '0';
+    signal reset        : std_logic := '0';
+    signal anode_7seg   : std_logic := '0';
+    signal cathode_7seg : std_logic_vector(7 downto 0);
 
 begin
 
-    dut: lfsr port map (clk => clk, reset => reset, Q => Q);
+    dut: top port map (clk          => clk,
+                       reset        => reset,
+                       anode_7seg   => anode_7seg,
+                       cathode_7seg => cathode_7seg);
 
     clk   <= not clk after 10 ns;
     reset <= '1', '0' after 100 ns;
